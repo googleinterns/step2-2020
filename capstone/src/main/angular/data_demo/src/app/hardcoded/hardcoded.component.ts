7b0896fe816ade8ce31b484bd7d8fe1bfdb55c71
Adedeji <ziniblessed@google.com>
2020-06-29 16:15:39 +0000

    reset previous commit

---
+++ capstone/src/main/angular/data_demo/src/app/hardcoded/hardcoded.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hardcoded',
+  templateUrl: './hardcoded.component.html',
+  styleUrls: ['./hardcoded.component.css']
+})
+export class HardcodedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    }
+}
