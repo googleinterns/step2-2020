fc8280b183ff27b04660536ce51be0bb7ee075da
Adedeji <ziniblessed@google.com>
2020-07-24 16:51:54 +0000

    Fixed flaky raw numbers toggle option on explore page

--- capstone/src/main/java/com/google/sps/data/AndroidManifestParser.java
+++ capstone/src/main/java/com/google/sps/data/AndroidManifestParser.java
@@ -2,12 +2,23 @@ package com.google.sps.data;
 
 public class AndroidManifestParser {
 
-    public static void main(String[] args) {    
-      AndroidManifestParser ManifestParser = new AndroidManifestParser();
-    }
+
     public void displayAndroidManifestContent(byte[] byteFile){
-        for (int i = 0; i < byteFile.length; i++) {
-            System.out.print((char) byteFile[i]);
-        }
+        String androidPermission= "android.permission";
+        int lnt = androidPermission.length();
+        String compare = "";
+        // for (int i = 0; i < byteFile.length; i++) {
+        //         compare += ((char)byteFile[i]);
+        //     if(i % lnt!=0){
+        //             //System.out.print(compare);   
+        //         if(androidPermission != compare){
+        //             System.out.print(compare);   
+        //             //System.out.print((char) byteFile[i]);
+        //         }
+                     
+        //         compare ="";
+        //     }
+                     
+        // }
     }
 }
