dabb62ec0286a6cbee041379aebdb4547d0debc0
Adedeji Adebisi <65958701+ziniblesssed@users.noreply.github.com>
2020-07-24 11:54:49 -0500

    Update AndroidManifestParser.java

--- capstone/src/main/java/com/google/sps/data/AndroidManifestParser.java
+++ capstone/src/main/java/com/google/sps/data/AndroidManifestParser.java
@@ -4,21 +4,6 @@ public class AndroidManifestParser {
 
 
     public void displayAndroidManifestContent(byte[] byteFile){
-        String androidPermission= "android.permission";
-        int lnt = androidPermission.length();
-        String compare = "";
-        // for (int i = 0; i < byteFile.length; i++) {
-        //         compare += ((char)byteFile[i]);
-        //     if(i % lnt!=0){
-        //             //System.out.print(compare);   
-        //         if(androidPermission != compare){
-        //             System.out.print(compare);   
-        //             //System.out.print((char) byteFile[i]);
-        //         }
-                     
-        //         compare ="";
-        //     }
-                     
-        // }
+        //I will be putting logic here, but function receives AndroidManifest.xml content
     }
 }
